a27ea7c3be239213c1dcb6eb31f35b3cfa4749e0
Bart Mach <bart.mach@gmail.com>
2019-03-06 00:16:46 -0500

    p 32 parity py - third solution using caching sub words

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,5 +1,8 @@
 from test_framework import generic_test
 
+words = {}
+word_size = 16
+bit_mask = pow(2, word_size) -1
 
 def parity(x):
     # TODO - you fill in here.
@@ -11,6 +14,18 @@ def parity(x):
 
     return par
 
+def cached_parity(x):
+    # TODO - you fill in here.
+    parity_tracker = 0
+    for i in range(4):
+        sub_word = (x >> (word_size * i)) & bit_mask
+        if sub_word in words:
+            comparator = words[sub_word]
+        else:
+            comparator = words[sub_word] = parity(sub_word)
+        parity_tracker ^= comparator
+    return parity_tracker
+
 
 if __name__ == '__main__':
-    exit(generic_test.generic_test_main("parity.py", 'parity.tsv', parity))
+    exit(generic_test.generic_test_main("parity.py", 'parity.tsv', cached_parity))
