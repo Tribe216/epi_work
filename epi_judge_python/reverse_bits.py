715525d51424c6573aac961476052a67e505a715
Bart Mach <bart.mach@gmail.com>
2019-03-06 20:46:00 -0500

    p 37 reverse bits py - first try with scan

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -1,10 +1,13 @@
 from test_framework import generic_test
-
+from math import log
 
 def reverse_bits(x):
     # TODO - you fill in here.
-    return 0
-
+    result = 0
+    for i in range(64):
+        if (x >> i & 1):
+            result += pow(2, 63 - i)
+    return result
 
 if __name__ == '__main__':
     exit(
