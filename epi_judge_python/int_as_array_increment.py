41b5cdf50ac5510f6069c37fd6a4d63362c76b3d
Bart Mach <bart.mach@gmail.com>
2019-03-07 23:32:46 -0500

    p 54 increment big int

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -3,8 +3,16 @@ from test_framework import generic_test
 
 def plus_one(A):
     # TODO - you fill in here.
-    return []
+    a = A
+    for x in range(len(a)-1, -1, -1):
+        a[x] = (a[x] + 1) % 10
+        if a[x] != 0:
+            break
+        if x == 0:
+            a.insert(0, 1)
 
+    return a
+        
 
 if __name__ == '__main__':
     exit(
